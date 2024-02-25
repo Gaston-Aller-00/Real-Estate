22e98994f8c6c9d6b99cdd781d28cba041593cae
Gaston <gastonallerdev@gmail.com>
2024-02-25 20:53:38 -0300

    mode

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-// import { userRoute } from './routes/userRoute.js';
+import { userRoute } from './routes/userRoute.js';
 // import { residencyRoute } from './routes/residencyRoute.js';
 dotenv.config()
 
@@ -18,7 +18,7 @@ app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
 });
 
-// app.use('/api/user', userRoute)
+app.use('/api/user', userRoute)
 // app.use("/api/residency", residencyRoute)
 
 // db mongodb+srv://gastonallerdev:hola123@cluster0.c6jvl6v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
