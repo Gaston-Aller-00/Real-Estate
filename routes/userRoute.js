5b2a91a6546d5cbc03db5b163d1da5e476898e86
Gaston <gastonallerdev@gmail.com>
2024-03-08 12:59:05 -0300

    useRoute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,9 +9,10 @@ import {
 } from "../controllers/userCntrl.js";
 import jwtCheck from "../config/auth0Config.js";
 const router = express.Router();
+
 router.post("/register", jwtCheck, createUser);
 router.post("/bookVisit/:id", jwtCheck, bookVisit);
-router.post("/getAllBookings", jwtCheck, getAllBookings);
+router.post("/allBookings", getAllBookings);
 router.post("/removeBooking/:id", jwtCheck, cancelBooking);
 router.post("/toFav/:rid", jwtCheck, toFav);
 router.post("/allFav/", jwtCheck, getAllFavorites);
